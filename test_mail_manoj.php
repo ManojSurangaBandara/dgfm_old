9fd1049c2252646a3b692fbf51814f87190d2d99
Manoj Bandara <manojband7@gmail.com>
2023-05-02 10:24:39 +0530

    test mail change 2

--- test_mail_manoj.php
+++ test_mail_manoj.php
@@ -27,30 +27,46 @@ require_once ('PHPMailer/Exception.php');
 
 
 //Army Mail ---------------------------------------------------
+// $phpmailer = new PHPMailer();
+// $phpmailer->isSMTP();
+// $phpmailer->Host = '172.16.0.18';
+// $phpmailer->SMTPAuth = true;
+// $phpmailer->Port = 587;
+// $phpmailer->SMTPSecure = 'tls'; // Use 'ssl' if you have an older version of PHPMailer
+// // $phpmailer->SMTPOptions = array(
+// // 'ssl' => array(
+// // 'verify_peer' => false,
+// // 'verify_peer_name' => false,
+// // 'allow_self_signed' => true
+// // )
+// // );
+// $phpmailer->Username = 'pradeep.s';
+// $phpmailer->Password = 'Army@1234';
+// $phpmailer->setFrom('pradeep.s@army.lk', 'Pradeep S');
+// $phpmailer->addAddress('suranga@army.lk');           // Add a recipient
+// $phpmailer->Subject = 'My Email subject';
+// $phpmailer->Body    = 'My Email body';
+// $attachment = 'excel/0AgeAnalys_Report.xls';
+// $phpmailer->addAttachment($attachment, 'RenamedFile.xls');
+//-------------------------------------------------------------
+
+
+
+//Mailtrap ---------------------------------------------------
 $phpmailer = new PHPMailer();
 $phpmailer->isSMTP();
-$phpmailer->Host = '172.16.0.18';
+$phpmailer->Host = 'sandbox.smtp.mailtrap.io';
 $phpmailer->SMTPAuth = true;
-$phpmailer->Port = 587;
-$phpmailer->SMTPSecure = 'tls'; // Use 'ssl' if you have an older version of PHPMailer
-// $phpmailer->SMTPOptions = array(
-// 'ssl' => array(
-// 'verify_peer' => false,
-// 'verify_peer_name' => false,
-// 'allow_self_signed' => true
-// )
-// );
-$phpmailer->Username = 'pradeep.s';
-$phpmailer->Password = 'ArRmy@1234';
-$phpmailer->setFrom('pradeep.s@army.lk', 'Pradeep S');
+$phpmailer->Port = 2525;
+$phpmailer->Username = '8370157732f009';
+$phpmailer->Password = '1a47e215521cc9';
+$phpmailer->setFrom('manoj@army.lk', 'Pradeep S');
 $phpmailer->addAddress('suranga@army.lk');           // Add a recipient
 $phpmailer->Subject = 'My Email subject';
 $phpmailer->Body    = 'My Email body';
 $attachment = 'excel/0AgeAnalys_Report.xls';
 $phpmailer->addAttachment($attachment, 'RenamedFile.xls');
 //-------------------------------------------------------------
-
-
 
 
 
